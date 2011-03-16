fa5d4a384f9d9baa2af4092108ee515e189d1be1
Roger Alsing <roger.alsing@precio.se>
2011-03-16 09:35:34 +0100

    Added NHibernate support

---
+++ Precio.Infrastructure.NHibernate/NHibernateUoW.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Linq;
+using NHibernate;
+
+namespace Precio.Domain
+{
+    public class NHibernateUoW : BaseUoW
+    {
+        private ISession session;
+        public NHibernateUoW(ISession session)
+        {
+            this.session = session;
+        }
+
+        public override void Add<T>(T aggregateRoot)
+        {
+            session.SaveOrUpdate(aggregateRoot);
+        }
+
+        public override void Remove<T>(T aggregateRoot)
+        {
+            session.Delete(aggregateRoot);
+        }
+
+        public override IQueryable<T> Query<T>()
+        {
+            return session.Linq<T>();
+        }
+
+        public override void Commit()
+        {
+            session.Flush();
+        }
+    }
+}
